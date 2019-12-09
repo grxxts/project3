8023790639ac169200af671070fe91f85a6adba6
grxxts <hectorarrieta14@gmail.com>
2019-12-09 10:54:55 +0100

    ok

--- client/src/guards/PrivateRoute.js
+++ client/src/guards/PrivateRoute.js
@@ -1,14 +1,17 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
-export default function PrivateRoute({component: Component, user, redirectPath, ...rest}) {
+export default function PrivateRoute({ component: Component, user, redirectPath, ...rest }) {
   return (
-   <Route {...rest} render={(props) => {
-    {if(user) {
-      return <Component loggedInUser={user} {...props}/>
-     } else {
-       return <Redirect to={{pathname: redirectPath}}/>
-     }
-    }
-   }} />
+    <Route {...rest} render={(props) => {
+      {
+        if (user) {
+          return <Component loggedInUser={user} {...props} />
+        } else {
+          return <Redirect to={{ pathname: redirectPath }} />
+        }
+      }
+          
+    }} />
+
   )
 }
