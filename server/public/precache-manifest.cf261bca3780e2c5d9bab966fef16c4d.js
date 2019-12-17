17fe796ff1c534140ec45be474995faadd83aed9
grxxts <hectorarrieta14@gmail.com>
2019-12-17 13:10:39 +0100

    still on heroku

---
+++ server/public/precache-manifest.cf261bca3780e2c5d9bab966fef16c4d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "97e73719c993f07c50222c9bb79f0aaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b58470d7c3c189d73d88",
+    "url": "/static/css/main.3172a6b2.chunk.css"
+  },
+  {
+    "revision": "11d2759429840a6ed0a0",
+    "url": "/static/js/2.46401016.chunk.js"
+  },
+  {
+    "revision": "1f97d805d3216826d3308143c5d83192",
+    "url": "/static/js/2.46401016.chunk.js.LICENSE"
+  },
+  {
+    "revision": "b58470d7c3c189d73d88",
+    "url": "/static/js/main.8b6e3e2f.chunk.js"
+  },
+  {
+    "revision": "2bcb8084869f23f72706",
+    "url": "/static/js/runtime-main.88d4f3f8.js"
+  }
+]);
